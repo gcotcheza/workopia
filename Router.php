2d420b585aece8f6229e0337c36964a0fae24aca
Ghie Cotcheza <ghiecotcheza@msml.nl>
2024-02-13 18:50:21 +0100

    add route class for structure

---
+++ Router.php
@@ -0,0 +1,79 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+
+    public function registerRoute(string $method, string $uri, string $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+        ];
+    }
+
+    /**
+     * Add a GET route.
+     */
+    public function get(string $uri, string $controller): void
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route.
+     */
+    public function post(string $uri, string $controller): void
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    /**
+     * Add a PATCH route.
+     */
+    public function patch(string $uri, string $controller): void
+    {
+        $this->registerRoute('PATCH', $uri, $controller);
+    }
+
+    /**
+     * Add a PUT route.
+     */
+    public function put(string $uri, string $controller): void
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    /**
+     * Add a DELETE route.
+     */
+    public function delete(string $uri, string $controller): void
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    /**
+     * Load error page
+     */
+    public function error(int $httpCode = 404): void
+    {
+        http_response_code($httpCode);
+        loadView("errors/{$httpCode}");
+        exit;
+    }
+    /**
+     * Route the request.
+     */
+    public function route(string $uri, string $method): void
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
+}
--- helpers.php
+++ helpers.php
@@ -3,55 +3,55 @@
 /**
  * Get the base path 
  */
- function basePath($path = ''): string
- {
-    return __DIR__ . '/' . $path;
- }
+function basePath(string $path = ''): string
+{
+   return __DIR__ . '/' . $path;
+}
 
- /**
-  * Load a view
-  */
-  function loadView($name): void
-  {
-      $viewPath = basePath("views/{$name}.view.php");
+/**
+ * Load a view
+ */
+function loadView(string $name): void
+{
+   $viewPath = basePath("views/{$name}.view.php");
 
-      if (file_exists($viewPath)) {
-         require $viewPath;
-      } else {
-         echo "View {$name} not found!";
-      }
-  }
-
-   /**
-  * Load a partial
-  */
-  function loadPartial($name): void
-  {
-      $prtialPath = basePath("views/partials/{$name}.php");
-
-      if (file_exists($prtialPath)) {
-         require $prtialPath;
-      } else {
-         echo "View {$name} not found!";
-      }
-  }
-
-  /**
-   * Inspect a value(s)
-   */
-   function inspect($value): void 
-   {
-      echo '<pre>';
-      var_dump($value);
-      echo '</pre>';
+   if (file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View {$name} not found!";
    }
+}
 
-   /**
-   * Inspect a value(s) and die
-   */
-   function inspectAndDie($value): void 
-   {
-      echo '<pre>';
-      die(var_dump($value));
-      echo '</pre>';
+/**
+ * Load a partial
+ */
+function loadPartial(string $name): void
+{
+   $prtialPath = basePath("views/partials/{$name}.php");
+
+   if (file_exists($prtialPath)) {
+      require $prtialPath;
+   } else {
+      echo "View {$name} not found!";
    }
+}
+
+/**
+ * Inspect a value(s)
+ */
+function inspect(mixed $value): void
+{
+   echo '<pre>';
+   var_dump($value);
+   echo '</pre>';
+}
+
+/**
+ * Inspect a value(s) and die
+ */
+function inspectAndDie(mixed $value): void
+{
+   echo '<pre>';
+   die(var_dump($value));
+   echo '</pre>';
+}
