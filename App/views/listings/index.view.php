4971a86eadaa3a1c4b05e2db7204389317958b96
Ghie Cotcheza <ghiecotcheza@msml.nl>
2024-02-26 15:35:19 +0100

    Add search functionality

--- App/views/listings/index.view.php
+++ App/views/listings/index.view.php
@@ -6,7 +6,17 @@
 
 <section>
     <div class="container mx-auto p-4 mt-4">
-        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">All Jobs</div>
+        <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">
+            <?php if (!empty($keywords) && !empty($location)) : ?>
+                Search result for: <?= htmlspecialchars($keywords) . ' and ' .  htmlspecialchars($location) ?>
+            <?php elseif (!empty($keywords)) : ?>
+                Search result for: <?= htmlspecialchars($keywords) ?>
+            <?php elseif (!empty($location)) : ?>
+                Search result for: <?= htmlspecialchars($location) ?>
+            <?php else : ?>
+                All Jobs
+            <?php endif; ?>
+        </div>
         <?= loadPartial('message') ?>
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             <?php foreach ($listings as $listing) : ?>
